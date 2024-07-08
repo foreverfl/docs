10c886bf0f76cadc37333b66962f95594e7139a8
foreverfl <forever_fl@naver.com>
2024-07-09 01:10:10 +0900

    Update nav color from white to black on general mode

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -24,7 +24,7 @@ const Logo = () => {
               d="M10 3v4a1 1 0 0 1-1 1H5m4 8h6m-6-4h6m4-8v16a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V7.914a1 1 0 0 1 .293-.707l3.914-3.914A1 1 0 0 1 9.914 3H18a1 1 0 0 1 1 1Z"
             />
           </svg>
-          <span className="min-w-32 text-2xl md:text-3xl truncate text-center font-navbar dark:text-slate-50 px-5 select-none">
+          <span className="min-w-32 text-2xl md:text-3xl truncate text-center font-navbar text-black dark:text-slate-50 px-5 select-none">
             mogumogu&#39;s docs
           </span>
         </div>

d0d99d8d22260c06f1a4cdabea9feaa04074340f
foreverfl <forever_fl@naver.com>
2025-05-08 23:08:37 +0900

    feat: Add publicRuntimeConfig in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,13 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: '/docs', 
-  assetPrefix: '/docs', 
+  basePath: '/docs',
+  assetPrefix: '/docs',
   images: {
     unoptimized: true,
+  },
+  publicRuntimeConfig: {
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '', 
   },
 };
 
